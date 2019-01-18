e910d0480c39596dd6c5ac767800fe1878861f36
Spencer McDonough <SwaggySpencerMcDee@Spencers-MacBook-Air-3.local>
2019-01-17 17:32:56 -0800

    working blerp, no edge cases

--- readraw.cpp
+++ readraw.cpp
@@ -1,9 +1,9 @@
 // This sample code reads in image data from a RAW image file and 
 // writes it into another file
 
-// NOTE:	The code assumes that the image is of size 256 x 256 and is in the
+// NOTE:	The code assumes that the image is of width 256 x 256 and is in the
 //			RAW format. You will need to make corresponding changes to
-//			accommodate images of different sizes and/or types
+//			accommodate images of different widths and/or types
 
 #include <stdio.h>
 #include <iostream>
@@ -14,7 +14,7 @@ using namespace std;
 /* QUESTIONS for TA
 1. do I have the right idea for the implementation of bilinear interpolation?
 */
-void blerp (/*unsigned char (*Imagedata)[Size][Size], unsigned char (*destImageData)[Size][3]*/)
+void blerp (/*unsigned char (*Imagedata)[width][width], unsigned char (*destImageData)[width][3]*/)
 {
 	// 	// assume layout:
 	// /*
@@ -24,9 +24,9 @@ void blerp (/*unsigned char (*Imagedata)[Size][Size], unsigned char (*destImageD
 	// 2 G R G R
 	// 3 B G B G
 	// */
-	// for (int i = 0; i < Size; i++)
+	// for (int i = 0; i < width; i++)
 	// {
-	// 	for (int j = 0; j < Size; j++)
+	// 	for (int j = 0; j < width; j++)
 	// 	{ 
 	// 		/* Case 1 (green center 1)
 	// 		G B G
@@ -109,39 +109,46 @@ int main(int argc, char *argv[])
 {
 	// Define file pointer and variables
 	FILE *file;
-	int BytesPerPixel;
-	int Size = 256;
+	int BytesPerPixel, BytesPerPixelOut;
+	int width, height;
 	
 	// Check for proper syntax
-	if (argc < 3){
+	if (argc < 3)
+	{
 		cout << "Syntax Error - Incorrect Parameter Usage:" << endl;
-		cout << "program_name input_image.raw output_image.raw [BytesPerPixel = 1] [Size = 256]" << endl;
+		cout << "program_name input_image.raw output_image.raw [BytesPerPixel = 1] [width = 256] [height = 256] [BytesPerPixelOut = 1]" << endl;
 		return 0;
 	}
 	
 	// Check if image is grayscale or color
-	if (argc < 4){
-		BytesPerPixel = 1; // default is grey image
-	} 
-	else {
-		BytesPerPixel = atoi(argv[3]);
-		// Check if size is specified
-		if (argc >= 5){
-			Size = atoi(argv[4]);
-		}
-	}
+	if (argc < 4) BytesPerPixel = 1; // default is grey image
+	else BytesPerPixel = atoi(argv[3]);
+
+	// Check if width is specified
+	if (argc < 5) width = 256;
+	else width = atoi(argv[4]);
+
+	// check if height is specified
+	if (argc < 6) height = 256;
+	else height = atoi(argv[5]);
+
+	// check if BytesPerPixelOut is specified
+	if (argc < 7) BytesPerPixelOut = 1;
+	else BytesPerPixelOut = atoi(argv[6]);
+	
 	
 	// Allocate source image data array
-	unsigned char Imagedata[Size][Size][BytesPerPixel];
+	unsigned char Imagedata[height][width][BytesPerPixel];
 	// Allocate dest image data array
-	unsigned char destImageData[Size][Size][3];
+	unsigned char destImageData[height][width][BytesPerPixelOut];
 
 	// Read image (filename specified by first argument) into image data matrix
-	if (!(file=fopen(argv[1],"rb"))) {
+	if (!(file=fopen(argv[1],"rb"))) 
+	{
 		cout << "Cannot open file: " << argv[1] <<endl;
 		exit(1);
 	}
-	fread(Imagedata, sizeof(unsigned char), Size*Size*BytesPerPixel, file);
+	fread(Imagedata, sizeof(unsigned char), height*width*BytesPerPixel, file);
 	fclose(file);
 
 	///////////////////////// INSERT YOUR PROCESSING CODE HERE /////////////////////////
@@ -154,9 +161,9 @@ int main(int argc, char *argv[])
 	2 G R G R
 	3 B G B G
 	*/
-	for (int i = 0; i < Size; i++)
+	for (int i = 1; i < height-1; i++)
 	{
-		for (int j = 0; j < Size; j++)
+		for (int j = 1; j < width-1; j++)
 		{ 
 			/* Case 1 (green center 1)
 			G B G
@@ -165,20 +172,13 @@ int main(int argc, char *argv[])
 			*/
 			if ((i % 2 == 0) && (j % 2 == 0))
 			{
-				if ((i != 0) && (j != 0))
-				{
-					// R = 0, G = 1, B = 2
-					// green = center pixel
-					destImageData[i][j][1] = Imagedata[i][j][1];
-					// red = average of left and right
-					destImageData[i][j][0] = (unsigned char)(0.5*((float)Imagedata[i-1][j][0] + (float)Imagedata[i+1][j][0]));
-					// blue = average of top and bottom
-					destImageData[i][j][2] = (unsigned char)(0.5*((float)Imagedata[i][j+1][0] + (float)Imagedata[i][j-1][0]));
-				}
-				else
-				{
-					
-				}
+				// R = 0, G = 1, B = 2
+				// green = center pixel
+				destImageData[i][j][1] = Imagedata[i][j][0];
+				// red = average of left and right
+				destImageData[i][j][0] = (unsigned char)(0.5*((float)Imagedata[i][j-1][0] + (float)Imagedata[i][j+1][0]));
+				// blue = average of top and bottom
+				destImageData[i][j][2] = (unsigned char)(0.5*((float)Imagedata[i+1][j][0] + (float)Imagedata[i-1][j][0]));
 			}
 			/* Case 2 (green center 2)
 			G R G
@@ -189,32 +189,32 @@ int main(int argc, char *argv[])
 			{
 				// R = 0, G = 1, B = 2
 				// green = center pixel
-				destImageData[i][j][1] = Imagedata[i][j][1];
+				destImageData[i][j][1] = Imagedata[i][j][0];
 				// blue = average of left and right
-				destImageData[i][j][2] = (unsigned char)(0.5*((float)Imagedata[i-1][j][0] + (float)Imagedata[i+1][j][0]));
+				destImageData[i][j][2] = (unsigned char)(0.5*((float)Imagedata[i][j-1][0] + (float)Imagedata[i][j+1][0]));
 				// red = average of top and bottom
-				destImageData[i][j][0] = (unsigned char)(0.5*((float)Imagedata[i][j+1][0] + (float)Imagedata[i][j-1][0]));
+				destImageData[i][j][0] = (unsigned char)(0.5*((float)Imagedata[i+1][j][0] + (float)Imagedata[i-1][j][0]));
 			}
 			/* Case 3 (red center)
 			B G B
 			G R G
 			B G B
 			*/
-			else if ((i % 2 == 1) && (j % 2 == 0))
+			else if ((i % 2 == 0) && (j % 2 == 1))
 			{
 				// R = 0, G = 1, B = 2
 				// red = center pixel
 				destImageData[i][j][0] = Imagedata[i][j][0];
 				// green = average of top, bottom, left, and right
 				destImageData[i][j][1] = (unsigned char)(0.25*((float)Imagedata[i-1][j][0] + 
-																(float)Imagedata[i+1][j][0]) +
+																(float)Imagedata[i+1][j][0]+
 																(float)Imagedata[i][j+1][0] + 
-																(float)Imagedata[i][j-1][0]);
+																(float)Imagedata[i][j-1][0]));
 				// blue = average of topLeft, topRight, bottomLeft, and bottomRight
 				destImageData[i][j][2] = (unsigned char)(0.25*((float)Imagedata[i-1][j-1][0] + 
-																(float)Imagedata[i+1][j-1][0]) +
+																(float)Imagedata[i+1][j-1][0] +
 																(float)Imagedata[i-1][j+1][0] + 
-																(float)Imagedata[i+1][j+1][0]);
+																(float)Imagedata[i+1][j+1][0]));
 			}
 			/* Case 4 (blue center)
 			R G R
@@ -224,18 +224,18 @@ int main(int argc, char *argv[])
 			else if ((i % 2 == 1) && (j % 2 == 0))
 			{
 				// R = 0, G = 1, B = 2
-				// red = center pixel
+				// blue = center pixel
 				destImageData[i][j][2] = Imagedata[i][j][0];
 				// green = average of top, bottom, left, and right
 				destImageData[i][j][1] = (unsigned char)(0.25*((float)Imagedata[i-1][j][0] + 
-																(float)Imagedata[i+1][j][0]) +
+																(float)Imagedata[i+1][j][0] +
 																(float)Imagedata[i][j+1][0] + 
-																(float)Imagedata[i][j-1][0]);
+																(float)Imagedata[i][j-1][0]));
 				// red = average of topLeft, topRight, bottomLeft, and bottomRight
 				destImageData[i][j][0] = (unsigned char)(0.25*((float)Imagedata[i-1][j-1][0] + 
-																(float)Imagedata[i+1][j-1][0]) +
+																(float)Imagedata[i+1][j-1][0] +
 																(float)Imagedata[i-1][j+1][0] + 
-																(float)Imagedata[i+1][j+1][0]);
+																(float)Imagedata[i+1][j+1][0]));
 			}
 		}
 	}
@@ -245,7 +245,7 @@ int main(int argc, char *argv[])
 		cout << "Cannot open file: " << argv[2] << endl;
 		exit(1);
 	}
-	fwrite(destImageData, sizeof(unsigned char), Size*Size*3, file);
+	fwrite(destImageData, sizeof(unsigned char), height*width*BytesPerPixelOut, file);
 	fclose(file);
 
 	return 0;
