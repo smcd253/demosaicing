723b50a9278b4eb9ae2e88b521521c8a60e6c373
Spencer McDonough <SwaggySpencerMcDee@Spencers-MacBook-Air-3.local>
2019-01-17 22:02:05 -0800

    start boundary cases

--- readraw.cpp
+++ readraw.cpp
@@ -8,102 +8,12 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
+#include "demosaic.h"
 
 using namespace std;
 
 /* QUESTIONS for TA
-1. do I have the right idea for the implementation of bilinear interpolation?
 */
-void blerp (/*unsigned char (*Imagedata)[width][width], unsigned char (*destImageData)[width][3]*/)
-{
-	// 	// assume layout:
-	// /*
-	//   0 1 2 3
-	// 0 G R G R
-	// 1 B G B G
-	// 2 G R G R
-	// 3 B G B G
-	// */
-	// for (int i = 0; i < width; i++)
-	// {
-	// 	for (int j = 0; j < width; j++)
-	// 	{ 
-	// 		/* Case 1 (green center 1)
-	// 		G B G
-	// 		R G R
-	// 		G B G
-	// 		*/
-	// 		if ((i % 2 == 0) && (j % 2 == 0))
-	// 		{
-	// 			// R = 0, G = 1, B = 2
-	// 			// green = center pixel
-	// 			destImageData[i][j][1] = Imagedata[i][j][1];
-	// 			// red = average of left and right
-	// 			destImageData[i][j][0] = (unsigned char)(0.5*((float)Imagedata[i-1][j][0] + (float)Imagedata[i+1][j][0]));
-	// 			// blue = average of top and bottom
-	// 			destImageData[i][j][2] = (unsigned char)(0.5*((float)Imagedata[i][j+1][0] + (float)Imagedata[i][j-1][0]));
-
-	// 		}
-	// 		/* Case 2 (green center 2)
-	// 		G R G
-	// 		B G B
-	// 		G R G
-	// 		*/
-	// 		else if ((i % 2 == 1) && (j % 2 == 1))
-	// 		{
-	// 			// R = 0, G = 1, B = 2
-	// 			// green = center pixel
-	// 			destImageData[i][j][1] = Imagedata[i][j][1];
-	// 			// blue = average of left and right
-	// 			destImageData[i][j][2] = (unsigned char)(0.5*((float)Imagedata[i-1][j][0] + (float)Imagedata[i+1][j][0]));
-	// 			// red = average of top and bottom
-	// 			destImageData[i][j][0] = (unsigned char)(0.5*((float)Imagedata[i][j+1][0] + (float)Imagedata[i][j-1][0]));
-	// 		}
-	// 		/* Case 3 (red center)
-	// 		B G B
-	// 		G R G
-	// 		B G B
-	// 		*/
-	// 		else if ((i % 2 == 1) && (j % 2 == 0))
-	// 		{
-	// 			// R = 0, G = 1, B = 2
-	// 			// red = center pixel
-	// 			destImageData[i][j][0] = Imagedata[i][j][0];
-	// 			// green = average of top, bottom, left, and right
-	// 			destImageData[i][j][1] = (unsigned char)(0.25*((float)Imagedata[i-1][j][0] + 
-	// 															(float)Imagedata[i+1][j][0]) +
-	// 															(float)Imagedata[i][j+1][0] + 
-	// 															(float)Imagedata[i][j-1][0]);
-	// 			// blue = average of topLeft, topRight, bottomLeft, and bottomRight
-	// 			destImageData[i][j][2] = (unsigned char)(0.25*((float)Imagedata[i-1][j-1][0] + 
-	// 															(float)Imagedata[i+1][j-1][0]) +
-	// 															(float)Imagedata[i-1][j+1][0] + 
-	// 															(float)Imagedata[i+1][j+1][0]);
-	// 		}
-	// 		/* Case 4 (blue center)
-	// 		R G R
-	// 		G B G
-	// 		R G R
-	// 		*/
-	// 		else if ((i % 2 == 1) && (j % 2 == 0))
-	// 		{
-	// 			// R = 0, G = 1, B = 2
-	// 			// red = center pixel
-	// 			destImageData[i][j][2] = Imagedata[i][j][0];
-	// 			// green = average of top, bottom, left, and right
-	// 			destImageData[i][j][1] = (unsigned char)(0.25*((float)Imagedata[i-1][j][0] + 
-	// 															(float)Imagedata[i+1][j][0]) +
-	// 															(float)Imagedata[i][j+1][0] + 
-	// 															(float)Imagedata[i][j-1][0]);
-	// 			// red = average of topLeft, topRight, bottomLeft, and bottomRight
-	// 			destImageData[i][j][0] = (unsigned char)(0.25*((float)Imagedata[i-1][j-1][0] + 
-	// 															(float)Imagedata[i+1][j-1][0]) +
-	// 															(float)Imagedata[i-1][j+1][0] + 
-	// 															(float)Imagedata[i+1][j+1][0]);
-	// 		}
-	// 	}
-	// }
-}
 
 int main(int argc, char *argv[])
 {
@@ -161,16 +71,50 @@ int main(int argc, char *argv[])
 	2 G R G R
 	3 B G B G
 	*/
-	for (int i = 1; i < height-1; i++)
+	for (int i = 0; i < height; i++)
 	{
-		for (int j = 1; j < width-1; j++)
+		for (int j = 0; j < width; j++)
 		{ 
+			// boundary cases
+			// top row
+			if ((i == 0))
+			{
+				// top left corner (green)
+				if (j == 0)
+				{
+					
+				}
+				else if (j != width - 1)
+				{
+
+				}
+				else
+				{
+					
+				}
+			}
+			// bottom left corner
+			else if ((i == height - 1) && (j == 0))
+			{
+
+			}
+			// top right corner
+			else if ((i == 0) && (j == width - 1))
+			{
+
+			}
+			// bottom right corner
+			else if ((i == height - 1) && (j == width - 1))
+			{
+
+			}
+			// interior cases
 			/* Case 1 (green center 1)
 			G B G
 			R G R
 			G B G
 			*/
-			if ((i % 2 == 0) && (j % 2 == 0))
+			else if ((i % 2 == 0) && (j % 2 == 0))
 			{
 				// R = 0, G = 1, B = 2
 				// green = center pixel
@@ -240,11 +184,12 @@ int main(int argc, char *argv[])
 		}
 	}
 	// Write image data (filename specified by second argument) from image data matrix
-
-	if (!(file=fopen(argv[2],"wb"))) {
+	if (!(file=fopen(argv[2],"wb"))) 
+	{
 		cout << "Cannot open file: " << argv[2] << endl;
 		exit(1);
 	}
+
 	fwrite(destImageData, sizeof(unsigned char), height*width*BytesPerPixelOut, file);
 	fclose(file);
 
